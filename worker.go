38075e65f6b933def1f6626c696a96875fffa209
shidenggui <903618848@qq.com>
2016-05-24 13:57:16 +0800

    feat(worker): worker add panic

--- worker.go
+++ worker.go
@@ -19,35 +19,40 @@ var TaskChan = make(chan []byte)
 
 func (w *Worker) Start() {
 	for {
-		taskStruct := new(Task)
-		taskByte := <-TaskChan
-		_ = json.Unmarshal(taskByte, &taskStruct)
-		argsMap := taskStruct.Args
-		log.Info(fmt.Sprintf("[WORKER] Receive task: %+v", taskStruct))
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Error(fmt.Sprintf("[WORKER] recover from panic: %v", r))
+				}
+			}()
+			taskStruct := new(Task)
+			taskByte := <-TaskChan
+			_ = json.Unmarshal(taskByte, &taskStruct)
+			argsMap := taskStruct.Args
+			log.Info(fmt.Sprintf("[WORKER] Receive task: %+v", taskStruct))
 
-		sender := w.Tasks[taskStruct.F]
-		res := sender.F(argsMap.(map[string]interface{}))
+			sender := w.Tasks[taskStruct.F]
+			res := sender.F(argsMap.(map[string]interface{}))
 
-		if taskStruct.Async {
-			log.Info(fmt.Sprintf("[WORKER] Finish task: %s, result: %v, async task no need reply", taskStruct.Id, res))
-			continue
-		}
+			if taskStruct.Async {
+				log.Info(fmt.Sprintf("[WORKER] Finish task: %s, result: %v, async task no need reply", taskStruct.Id, res))
+				return
+			}
 
-		log.Info(fmt.Sprintf("[WORKER] Finish task: %s, reply result: %v", taskStruct.Id, res))
+			log.Info(fmt.Sprintf("[WORKER] Finish task: %s, reply result: %v", taskStruct.Id, res))
 
-		replyJson, err := json.Marshal(res)
-		if err != nil {
-			log.Info(fmt.Sprintf("[WORKER] Encode task %s result:  to json err: %s", taskStruct.Id, res, err))
-			continue
-		}
-		err = w.Broker.Delay(replyJson, taskStruct.Id)
-		if err != nil {
-			log.Info(fmt.Sprintf("[WORKER] Reply task %s err: %s", taskStruct.Id, err))
-			continue
-		}
+			replyJson, err := json.Marshal(res)
+			if err != nil {
+				log.Info(fmt.Sprintf("[WORKER] Encode task %s result:  to json err: %s", taskStruct.Id, res, err))
+				return
+			}
+			err = w.Broker.Delay(replyJson, taskStruct.Id)
+			if err != nil {
+				log.Info(fmt.Sprintf("[WORKER] Reply task %s err: %s", taskStruct.Id, err))
+				return
+			}
 
-		w.Broker.Expire(taskStruct.Id, 1800)
+			w.Broker.Expire(taskStruct.Id, 1800)
+		}()
 	}
 }
-
-// func (w *Worker) Consume()
